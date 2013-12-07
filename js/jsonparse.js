975115595e7be58b65c6abf51752ccfa68ee28c1
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-12-07 02:40:45 -0500

    working on sending http requests.

--- js/jsonparse.js
+++ js/jsonparse.js
@@ -1,3 +1,13 @@
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
 function getGame(gameUrl)
 {
 	var xmlHttp = null;
@@ -7,3 +17,41 @@ function getGame(gameUrl)
     xmlHttp.send( null );
     console.log(xmlHttp.responseText);
 }
+
+function simpleHttpRequest(url) {
+  var request = makeHttpObject();
+  request.open("GET", url, true);
+  request.send(null);
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      if (request.status == 200)
+        console.log(request.responseText);
+      //else if (failure)
+       // failure(request.status, request.statusText);
+    }
+  };
+}
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
