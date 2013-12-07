7231996d2c169c6491dcf5101f616ce16f6cd69f
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-12-07 18:44:10 -0500

    testing local get server info.

--- js/jsonparse.js
+++ js/jsonparse.js
@@ -1,6 +1,6 @@
 //This file will handle all the server communication and json parsing. 
 
-function requestCrossDomain( site) {  
+function requestCrossDomain( site, gameOver) {  
      /*
      * To do around the cross domain probelm I used a yahooapi, which can be a little slow, but it shouldn't ever fail and it is
      * not as much additional code as other methods.
@@ -12,12 +12,13 @@ function requestCrossDomain( site) {
     }  
       
     // Take the provided url, and add it to a YQL query. Make sure you encode it!  
-    var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=json&callback=?';  
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=json&callback=?';  
     //  var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';  
     // Request that YSQL string, and run a callback function.  
     // Pass a defined function to prevent cache-busting.  
-    $.getJSON( yql, moveList );  
-      
+    
+    $.getJSON( yql, moveList);  
+   
     
     function cbFunc(data) {  
     // If we have something to work with... 
@@ -28,6 +29,17 @@ function requestCrossDomain( site) {
 
 function moveList(results)
 {
-	var turn = $.parseJSON((results.query.results.body.p));
-	console.log(turn);
+	//var turn = $.parseJSON((results.query.results.body.p));
+	console.log(results);
+
+}
+
+function getGame(gameUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", gameUrl, false );
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);
 }
