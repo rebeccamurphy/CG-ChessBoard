2521d97660975bb0685498dc5dfe542df66f63c5
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-12-07 17:20:06 -0500

    figured out how to get the json.

--- js/jquery.xdomainajax.js
+++ js/jquery.xdomainajax.js
@@ -40,7 +40,7 @@ jQuery.ajax = (function(_ajax){
                         (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
                     : '')
                 ),
-                format: 'json'
+                format: 'xml'
             };
             
             // Since it's a JSONP request
