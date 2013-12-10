55ed7c215feeb1e001293a4420daaf4fd4be22e2
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-12-10 13:48:46 -0500

    still just trying  to see debug messages.

--- js/initBoard.js
+++ js/initBoard.js
@@ -167,23 +167,26 @@ function animate() {
 
         if (gameid !== "None" && startGame === true)
         {
-                if (jsonobj.gameOver === false && currentTurn >= lastTurn && animationFlag !== 1)
+                if (jsonobj.gameover === false && currentTurn >= lastTurn && animationFlag !== 1)
                 {
-                        if ( jsonobj.lastmovenumber == currentTurn && jsonobj.gameover == false)
-                        {console.log('is getting to this if');
+                        
+                                console.log('is getting to this if');
                                 setTimeout(function (){
                                     jsonobj = getGame('https://10.11.18.65/cg/chess/' + gameid);
                                     console.log('trying to get new moves from server.');
-                                    if (jsonobj.moves != turnArray)
-                                        console.log('success!');
+                                    if (jsonobj.moves.length > turnArray.length)
+                                        {
+                                            console.log('success!');
+                                              turnArray = jsonobj.moves;
+                                            lastTurn = jsonobj.lastmovenumber;
+                                            updateTime(); }
                                     else 
                                         console.log('hafta try again');
-                                    turnArray = jsonobj.moves;
-                                    lastTurn = jsonobj.lastmovenumber;
-                                    updateTime();
+
+                                  
 
                                 }, 5000);
-                        }
+                          
                
                 }
                 else if (jsonobj.gameover === true && currentTurn >= lastTurn &&  animationFlag !== 1)
